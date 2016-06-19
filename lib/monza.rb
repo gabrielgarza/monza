6a1b8c3c3941a2738693faa7c080e5c4cd6dbb74
Gabriel <garzagabriel@gmail.com>
2016-06-19 01:24:56 -0700

    verify method moved

--- lib/monza.rb
+++ lib/monza.rb
@@ -3,26 +3,3 @@ require "monza/client"
 require "monza/verification_response"
 require "monza/receipt"
 require "monza/transaction_receipt"
-
-class Monza
-
-  def self.verify(data, options = {})
-    client = Client.production
-
-    begin
-      client.verify(data, options)
-    rescue VerificationError => error
-      case error.code
-      when 21007 # This receipt is from the test environment, but it was sent to the production environment for verification. Send it to the test environment instead.
-        client = Client.development
-        retry
-      when 21008 # This receipt is from the production environment, but it was sent to the test environment for verification. Send it to the production environment instead.
-        client = Client.production
-        retry
-      else
-        raise error
-      end
-    end
-  end
-
-end
--- lib/monza/receipt.rb
+++ lib/monza/receipt.rb
@@ -66,6 +66,25 @@ module Monza
       end
     end # end initialize
 
+    def self.verify(data, options = {})
+      client = Client.production
+
+      begin
+        client.verify(data, options)
+      rescue VerificationResponse::VerificationError => error
+        case error.code
+        when 21007 # This receipt is from the test environment, but it was sent to the production environment for verification. Send it to the test environment instead.
+          client = Client.development
+          retry
+        when 21008 # This receipt is from the production environment, but it was sent to the test environment for verification. Send it to the production environment instead.
+          client = Client.production
+          retry
+        else
+          raise error
+        end
+      end
+    end
+
   end # end class
 end # end module
 
