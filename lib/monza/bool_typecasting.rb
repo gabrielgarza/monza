34d26ab22552927e494ff9f849501a5c27a19a8b
Gabriel <garzagabriel@gmail.com>
2016-06-24 10:58:26 -0700

    empty instead of blank in String extension

--- lib/monza/bool_typecasting.rb
+++ lib/monza/bool_typecasting.rb
@@ -1,9 +1,10 @@
 class String
   def to_bool
     return true if self == true || self =~ (/^(true|t|yes|y|1)$/i)
-    return false if self == false || self.blank? || self =~ (/^(false|f|no|n|0)$/i)
+    return false if self == false || self.empty? || self =~ (/^(false|f|no|n|0)$/i)
     raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
   end
+  #  if using Rails empty? can be changed for blank?
 end
 
 class Fixnum
