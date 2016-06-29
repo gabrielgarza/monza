11deaf870a2fcf1f70bced79ea64685f111a31d6
Gabriel <garzagabriel@gmail.com>
2016-06-29 11:19:36 -0700

    test Time.zone.at for receipt spec

--- spec/receipt_spec.rb
+++ spec/receipt_spec.rb
@@ -25,13 +25,13 @@ describe Monza::Receipt do
     it { expect(receipt.bundle_id).to eq 'com.example.app' }
     it 'time zone' do
       expect(receipt.receipt_creation_date).to eq DateTime.parse('2016-06-17 01:54:26 Etc/GMT')
-      expect(receipt.receipt_creation_date_ms).to eq DateTime.parse('2016-06-17 01:54:26 Etc/GMT')
+      expect(receipt.receipt_creation_date_ms).to eq Time.zone.at("1466128466000".to_i / 1000)
 
       expect(receipt.request_date).to eq DateTime.parse('2016-06-17 17:34:41 Etc/GMT')
-      expect(receipt.request_date_ms).to eq DateTime.parse('2016-06-17 17:34:41 Etc/GMT')
+      expect(receipt.request_date_ms).to eq Time.zone.at("1466184881174".to_i / 1000)
 
       expect(receipt.original_purchase_date).to eq DateTime.parse('2013-08-01 07:00:00 Etc/GMT')
-      expect(receipt.original_purchase_date_ms).to eq DateTime.parse('2013-08-01 07:00:00 Etc/GMT')
+      expect(receipt.original_purchase_date_ms).to eq Time.zone.at("1375340400000".to_i / 1000)
     end
 
     it 'receipt' do
@@ -46,13 +46,16 @@ describe Monza::Receipt do
       expect(in_app.product_id).to eq 'com.example.product_id'
 
       expect(in_app.purchase_date).to eq DateTime.parse('2016-06-17 01:32:28 Etc/GMT')
-      expect(in_app.purchase_date_ms).to eq DateTime.parse('2016-06-17 01:32:28 Etc/GMT')
+      expect(in_app.purchase_date_ms).to eq Time.zone.at("1466127148000".to_i / 1000)
 
       expect(in_app.original_purchase_date).to eq DateTime.parse('2016-06-17 01:30:33 Etc/GMT')
-      expect(in_app.original_purchase_date_ms).to eq DateTime.parse('2016-06-17 01:30:33 Etc/GMT')
+      expect(in_app.original_purchase_date_ms).to eq Time.zone.at("1466127033000".to_i / 1000)
 
       expect(in_app.expires_date).to eq DateTime.parse('2016-06-17 01:37:28 Etc/GMT')
-      expect(in_app.expires_date_ms).to eq DateTime.parse('2016-06-17 01:37:28 Etc/GMT')
+      expect(in_app.expires_date_ms).to eq Time.zone.at("1466127448000".to_i / 1000)
+
+      expect(in_app.is_trial_period).to eq false
     end
+
   end
 end
