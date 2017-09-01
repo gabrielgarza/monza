69cdb162334d16a565126b6d4a267a006c13f0ae
Takuya Okuhara <okutaku0507@gmail.com>
2017-09-01 11:22:05 +0900

    Add rspec for Monza config

--- spec/monza_spec.rb
+++ spec/monza_spec.rb
@@ -5,6 +5,11 @@ describe Monza do
     expect(Monza::VERSION).not_to be nil
   end
 
+  it 'time zone' do
+    expect(Thread.current[:time_zone]).to be nil
+    expect(Time.zone_default).not_to be nil
+  end
+
   # it 'does something useful' do
   #   expect(false).to eq(true)
   # end
