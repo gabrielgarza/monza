0bcfd120c92d19cbc6e54f84511b3261ffaa0771
Gabriel Garza <garzagabriel@gmail.com>
2019-10-29 21:42:56 -0700

    fix broken spec

--- spec/renewal_spec.rb
+++ spec/renewal_spec.rb
@@ -11,9 +11,7 @@ describe Monza::RenewalInfo do
     it { expect(renewal_info.will_renew).to eq false }
     it { expect(renewal_info.is_in_billing_retry_period).to eq false }
     it { expect(renewal_info.auto_renew_product_id).to eq "renew_product_id" }
-    it do
-      expect(renewal_info.grace_period_purchase_date).to eq DateTime.parse('2013-08-01 07:00:00 Etc/GMT')
-      expect(renewal_info.grace_period_purchase_date_ms).to eq Time.zone.at("1375340400000".to_i / 1000)
-    }
+    it { expect(renewal_info.grace_period_purchase_date).to eq DateTime.parse('2013-08-01 07:00:00 Etc/GMT') }
+    it { expect(renewal_info.grace_period_purchase_date_ms).to eq Time.zone.at("1375340400000".to_i / 1000) }
   end
 end
