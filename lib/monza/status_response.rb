76496b658bebb8ed68fd2d16adc8600d94e6b8b4
Peter Kovacs <peter@sanebox.com>
2020-07-17 15:14:25 -0400

    Add status notification types.

--- lib/monza/status_response.rb
+++ lib/monza/status_response.rb
@@ -5,6 +5,18 @@ module Monza
   class StatusResponse
     # https://developer.apple.com/library/ios/releasenotes/General/ValidateAppStoreReceipt/Chapters/ValidateRemotely.html
     # https://developer.apple.com/documentation/appstoreservernotifications/responsebody
+    module Type 
+      CANCEL = 'CANCEL'
+      DID_CHANGE_RENEWAL_PREF = 'DID_CHANGE_RENEWAL_PREF'
+      DID_CHANGE_RENEWAL_STATUS = 'DID_CHANGE_RENEWAL_STATUS'
+      DID_FAIL_TO_RENEW = 'DID_FAIL_TO_RENEW'
+      DID_RECOVER = 'DID_RECOVER'
+      INITIAL_BUY = 'INITIAL_BUY'
+      INTERACTIVE_RENEWAL = 'INTERACTIVE_RENEWAL'
+      RENEWAL = 'RENEWAL'
+      REFUND = 'REFUND'
+    end
+
 
     attr_reader :auto_renew_product_id
     attr_reader :auto_renew_status
@@ -121,6 +133,42 @@ module Monza
       # The field is labelled "PST" by apple, but the "America/Los_Angelus" time zone is actually Pacific Time, 
       # which is different, because it observes DST.
       ActiveSupport::TimeZone["Pacific Time (US & Canada)"].parse(pt).to_datetime
+    end
+
+    def cancel?
+      notification_type == Type::CANCEL
+    end
+
+    def did_change_renewal_pref?
+      notification_type == Type::DID_CHANGE_RENEWAL_PREF
+    end
+
+    def did_change_renewal_status?
+      notification_type == Type::DID_CHANGE_RENEWAL_STATUS
+    end
+
+    def did_fail_to_renew?
+      notification_type == Type::DID_FAIL_TO_RENEW
+    end
+
+    def did_recover?
+      notification_type == Type::DID_RECOVER
+    end
+
+    def initial_buy?
+      notification_type == Type::INITIAL_BUY
+    end
+
+    def interactive_renewal?
+      notification_type == Type::INTERACTIVE_RENEWAL
+    end
+
+    def renewal?
+      notification_type == Type::RENEWAL
+    end
+
+    def refund?
+      notification_type == Type::REFUND
     end
 
   end # class
